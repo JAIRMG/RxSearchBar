ccec8d10d533d66e3510ac987052ac08d2cf4cdd
Jair Moreno Gaspar <jair.moreno@globant.com>
2019-04-11 15:28:55 -0500

    Initial Commit

---
+++ RxSearchBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSearchBar
+//
+//  Created by Jair Moreno Gaspar on 4/11/19.
+//  Copyright © 2019 Jair Moreno Gaspar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
